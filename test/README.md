2884310821809d235899e13b128d3b4e33a2a409
Austin King <shout@ozten.com>
2011-09-23 12:34:35 -0700

    Adding pluginviewer test

---
+++ test/README.md
@@ -0,0 +1,22 @@
+# Tests #
+
+The easiest way to run the tests is from within a Vagrant VM.
+
+## Dependencies ##
+
+These tests depend on the following:
+
+* Apache - fakes browser verification services
+* MySQL - plugin session store
+* OpenLDAP - SASL enabled LDAP directory server
+* Python - Tests sasl-browserid directly and indirectly.
+* SASL2-bin - Test programs and utilities from Cyrus SASL
+
+Each of these must be setup and running based on ../configs/
+
+## Running Tests ##
+
+Run the following command:
+
+    sudo python test/unit_privileged_test_suite.py
+    
---
+++ test/unit_privileged_test_suite.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+Privileged Unit Test Suite.
+
+These tests should be run inside of a Vagrant VM.
+
+They exercise the low level functions of the C plugin.
+
+A minimal set of programs has been written which expose
+units of code to the system as CLI. They are scripted below
+via Python's sys.exec to provide a convient way to test
+corner cases, etc.
+"""
+import re
+import subprocess
+import unittest
+
+
+class InstalledProperlyTestCase(unittest.TestCase):
+    def test_pluginviewer_sees_browserid(self):
+        p = subprocess.Popen(['saslpluginviewer'], stdout=subprocess.PIPE)
+        p.wait()
+        output = p.stdout.readlines()
+        seen = self._parse_pv(output)
+        self.assertTrue(seen['client'], 'Test run under sudo, right?')
+        self.assertTrue(seen['server'])
+
+    def _parse_pv(self, output):
+        """Examines pluginviewer output and looks for BROWSER-ID"""
+        in_client = False
+        in_server = False
+        seen = dict(client=False, server=False)
+        for l in output:
+            line = l.rstrip()
+            if "Installed SASL (server side) mechanisms are:" == line:
+                in_client = False
+                in_server = True
+            elif "Installed SASL (client side) mechanisms are:" == line:
+                in_client = True
+                in_server = False
+            elif re.match('Installed.*mechanisms are:$', line):
+                in_client = False
+                in_server = False
+            elif re.match('.*SASL mechanism: BROWSER-ID, .*', line):
+                if in_client:
+                    seen['client'] = True
+                elif in_server:
+                    seen['server'] = True
+                else:
+                    self.fail("Got an unexpected BROWSER-ID plugin [%s]" % line)
+            else:
+                pass
+        return seen
+
+if __name__ == '__main__':
+    unittest.main()
+
