b7c0bb06e0246400c1eec532aba744417e0dc8d5
Austin King <shout@ozten.com>
2011-12-15 16:43:56 -0800

    Removing syslog and using saslplug utils log. Fixes Issues#1 and 2

--- plugins/verifier.c
+++ plugins/verifier.c
@@ -2,7 +2,6 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <syslog.h>
 
 #include <curl/curl.h>
 #include <sasl/sasl.h> /* saslplug.h should have included this ?!? */
@@ -23,6 +22,7 @@ struct json_response {
   size_t size;
   size_t realsize;
   int memerr;
+  sasl_utils_t *utils;
 };
 
 /** Callback function for streaming CURL response */
@@ -32,24 +32,24 @@ static size_t write_cb(void *contents, size_t size, size_t nmemb, void *userp)
 	size_t nextsize;
 	struct json_response *mem = (struct json_response *)userp;
 
-        /** check for multiplication overflow */
-        if (size != 0 && nmemb != 0 && realsize < size) {
-            /** CURL spec says number of bytes handled should be returned
-             * http://curl.haxx.se/libcurl/c/curl_easy_setopt.html
-             */
-                syslog(LOG_ERR, "Integer Overflow early, ignoring new contents");
-                return 0;
-        }
+	/** check for multiplication overflow */
+	if (size != 0 && nmemb != 0 && realsize < size) {
+	    /** CURL spec says number of bytes handled should be returned
+	     * http://curl.haxx.se/libcurl/c/curl_easy_setopt.html
+	     */
+		mem->utils->log(NULL, SASL_LOG_ERR, "Integer Overflow early, ignoring new contents");
+		return 0;
+	}
 	if (mem->size + realsize >= mem->realsize) {
 		nextsize = mem->size + realsize + JSON_BUFFER;
 		if (nextsize < mem->realsize) {
-			syslog(LOG_ERR, "Integer Overflow, ignoring new contents");
+			mem->utils->log(NULL, SASL_LOG_ERR, "Integer Overflow, ignoring new contents");
 			return 0;
 		}
 		mem->realsize = nextsize;
 		void *tmp = malloc(mem->size + realsize + JSON_BUFFER);
 		if (tmp == NULL) {
-			syslog(LOG_ERR, "Unable to grow json_response tmp buffer");
+			mem->utils->log(NULL, SASL_LOG_ERR, "Unable to grow json_response tmp buffer");
 			mem->memerr = 1;
 			return 0;
 		}
@@ -57,7 +57,7 @@ static size_t write_cb(void *contents, size_t size, size_t nmemb, void *userp)
 		free(mem->memory);
 		mem->memory = malloc(mem->size + realsize + JSON_BUFFER);
 		if (mem->memory == NULL) {
-			syslog(LOG_ERR, "Unable to grow json_response memory slot");
+			mem->utils->log(NULL, SASL_LOG_ERR, "Unable to grow json_response memory slot");
 			mem->memerr = 1;
 			return 0;
 		}
@@ -72,7 +72,8 @@ static size_t write_cb(void *contents, size_t size, size_t nmemb, void *userp)
 }
 
 static int parse(const char* resp,
-		  struct browserid_response_t *browserid_response);
+		 struct browserid_response_t *browserid_response,
+		 const sasl_utils_t *utils);
 
 /**
  * Attempts to verify an assertion and audience against the
@@ -90,7 +91,7 @@ int browserid_verify(const sasl_utils_t *utils,
 	const char *bid_url;
 	char *bid_body;
 	char *bid_body_fmt = "assertion=%s&audience=%s";
-	struct json_response json_text;
+	struct json_response json_text; /* context */
 	char *resp;
 
 
@@ -103,7 +104,7 @@ int browserid_verify(const sasl_utils_t *utils,
 		    "https://browserid.org/verify";
 	}
 
-	syslog(LOG_INFO, "bidurl = %s", bid_url);
+	utils->log(NULL, SASL_LOG_NOTE, "bidurl = %s", bid_url);
 
 	bid_body = malloc(strlen(bid_body_fmt) +
 			  strlen(assertion) + strlen(audience) + 1);
@@ -112,7 +113,7 @@ int browserid_verify(const sasl_utils_t *utils,
 		return SASL_NOMEM;
 	}
 	sprintf(bid_body, bid_body_fmt, assertion, audience);
-	syslog(LOG_INFO, "bid_body = %d %s", strlen(bid_body), bid_body);
+	utils->log(NULL, SASL_LOG_NOTE, "bid_body = %d %s", strlen(bid_body), bid_body);
 
 	strcpy(browserid_response->status, "");
 	strcpy(browserid_response->email, "");
@@ -122,44 +123,45 @@ int browserid_verify(const sasl_utils_t *utils,
 	strcpy(browserid_response->reason, "");
 
 	if (0 != curl_global_init(CURL_GLOBAL_SSL)) {
-		syslog(LOG_ERR, "curl_global_init was non-zero");
+		utils->log(NULL, SASL_LOG_ERR, "curl_global_init was non-zero");
 		return SASL_FAIL;
 	}
 
 	handle = curl_easy_init();
 	if (handle == NULL) {
-		syslog(LOG_ERR, "Unable to curl_easy_init");
+		utils->log(NULL, SASL_LOG_ERR, "Unable to curl_easy_init");
 		return SASL_FAIL;
 	}
 	if (0 != curl_easy_setopt(handle, CURLOPT_URL, bid_url)) {
-		syslog(LOG_ERR, "curl setopt url failed");
+		utils->log(NULL, SASL_LOG_ERR, "curl setopt url failed");
 		return SASL_FAIL;
-        }
+	}
 	if (0 != curl_easy_setopt(handle, CURLOPT_POST, 1)) {
-		syslog(LOG_ERR, "curl setopt post failed");
+		utils->log(NULL, SASL_LOG_ERR, "curl setopt post failed");
 		return SASL_FAIL;
-        }
+	}
 	if (0 != curl_easy_setopt(handle, CURLOPT_POSTFIELDS, bid_body)) {
-		syslog(LOG_ERR, "curl setopt postfields failed");
+		utils->log(NULL, SASL_LOG_ERR, "curl setopt postfields failed");
 		return SASL_FAIL;
-        }
+	}
 	if (0 != curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1)) {
-		syslog(LOG_DEBUG, "curl setopt follow");
+		utils->log(NULL, SASL_LOG_DEBUG, "curl setopt follow");
 		return SASL_FAIL;
-        }
+	}
 	if (0 != curl_easy_setopt(handle, CURLOPT_USE_SSL, CURLUSESSL_ALL)) {
-		syslog(LOG_DEBUG, "curl setopt ssl failed");
+		utils->log(NULL, SASL_LOG_DEBUG, "curl setopt ssl failed");
 		return SASL_FAIL;
-        }
+	}
 	if (0 != curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION,
 				  write_cb)) {
-		syslog(LOG_ERR, "curl setopt write fn failed");
+		utils->log(NULL, SASL_LOG_ERR, "curl setopt write fn failed");
 		return SASL_FAIL;
-        }
+	}
 
 	json_text.size = 0;
 	json_text.memerr = 0;
 	json_text.realsize = JSON_BUFFER;
+	json_text.utils = utils;
 	json_text.memory = malloc(JSON_BUFFER);
 	if (json_text.memory == NULL) {
 		MEMERROR( utils );
@@ -167,9 +169,9 @@ int browserid_verify(const sasl_utils_t *utils,
 	}
 
 	if (0 != curl_easy_setopt(handle, CURLOPT_WRITEDATA, &json_text)) {
-		syslog(LOG_ERR, "curl setopt writedata failed");
+		utils->log(NULL, SASL_LOG_ERR, "curl setopt writedata failed");
 		return SASL_FAIL;
-        }
+	}
 
 	code = curl_easy_perform(handle);
 
@@ -179,17 +181,17 @@ int browserid_verify(const sasl_utils_t *utils,
 	}
 
 	if (code == 0) {
-		r = parse(json_text.memory, browserid_response);
+		r = parse(json_text.memory, browserid_response, utils);
 	} else {
-		syslog(LOG_EMERG, "curl_easy_perform failed [%u] %s", code,
+		utils->log(NULL, SASL_LOG_ERR, "curl_easy_perform failed [%u] %s", code,
 		       curl_easy_strerror(code));
 		strcpy(browserid_response->status, "curl-error");
-                if (strlen(curl_easy_strerror(code)) < MAX_RESP_FIELD) {
-                        strcpy(browserid_response->reason, curl_easy_strerror(code));
-                } else {
-                        syslog(LOG_ERR, curl_easy_strerror(code));
-                        strcpy(browserid_response->reason, "Curl failed, error message too large see syslog.");
-                }
+		if (strlen(curl_easy_strerror(code)) < MAX_RESP_FIELD) {
+			strcpy(browserid_response->reason, curl_easy_strerror(code));
+		} else {
+			utils->log(NULL, SASL_LOG_ERR, curl_easy_strerror(code));
+			strcpy(browserid_response->reason, "Curl failed, error message too large see syslog.");
+		}
 	}
 
 	curl_easy_cleanup(handle);
@@ -198,32 +200,33 @@ int browserid_verify(const sasl_utils_t *utils,
 }
 
 static int parse(const char* resp,
-		  struct browserid_response_t *browserid_response)
+		 struct browserid_response_t *browserid_response,
+		 const sasl_utils_t *utils)
 {
 	yajl_val tree = NULL;
 	char err_buf[256];
 
-	syslog(LOG_DEBUG, "beginning parse %s", resp);
+	utils->log(NULL, SASL_LOG_DEBUG, "beginning parse %s", resp);
 
 	tree = yajl_tree_parse(resp, err_buf, 255);
 
 	if (!tree) {
-		syslog(LOG_ERR, "bid resp=%s", resp);
-		syslog(LOG_ERR, "Error parsing BrowserID response [%s]",
+		utils->log(NULL, SASL_LOG_ERR, "bid resp=%s", resp);
+		utils->log(NULL, SASL_LOG_ERR, "Error parsing BrowserID response [%s]",
 		       err_buf);
 		return SASL_FAIL;
 	}
 
-	syslog(LOG_DEBUG, "Obtained parser tree");
+	utils->log(NULL, SASL_LOG_DEBUG, "Obtained parser tree");
 
 	const char *status_path[] = { "status", (const char *) 0 };
 	yajl_val status = yajl_tree_get(tree, status_path, yajl_t_string);
 	if (!status || strlen(status->u.string) >= MAX_STATUS) {
-		syslog(LOG_EMERG, "bid resp=%s", resp);
-		syslog(LOG_EMERG, "Expected field status is missing or too large");
+		utils->log(NULL, SASL_LOG_ERR, "bid resp=%s", resp);
+		utils->log(NULL, SASL_LOG_ERR, "Expected field status is missing or too large");
 		return SASL_FAIL;
 	}
-	syslog(LOG_DEBUG, "Obtained status %s", status->u.string);
+	utils->log(NULL, SASL_LOG_DEBUG, "Obtained status %s", status->u.string);
 
 	strcpy(browserid_response->status, status->u.string);
 
@@ -237,39 +240,39 @@ static int parse(const char* resp,
 
 		email = yajl_tree_get(tree, email_path, yajl_t_string);
 		if (!email || strlen(email->u.string) >= MAX_RESP_FIELD) {
-                        /* Can't continue without email */
-                        syslog(LOG_ERR, "bid resp=%s", resp);
-			syslog(LOG_ERR, "Expected field email is missing or too large.");
-                        return SASL_FAIL;
+			/* Can't continue without email */
+			utils->log(NULL, SASL_LOG_ERR, "bid resp=%s", resp);
+			utils->log(NULL, SASL_LOG_ERR, "Expected field email is missing or too large.");
+			return SASL_FAIL;
 		} else {
-                        strcpy(browserid_response->email, email->u.string);
+			strcpy(browserid_response->email, email->u.string);
 		}
 
 		audience = yajl_tree_get(tree, audience_path, yajl_t_string);
 		if (!audience) {
-			syslog(LOG_ERR, "Expected field audience is missing");
+			utils->log(NULL, SASL_LOG_ERR, "Expected field audience is missing");
 		} else {
-                        if (strlen(audience->u.string) < MAX_RESP_FIELD) {
-			        strcpy(browserid_response->audience, audience->u.string);
-                        } else {
-                                syslog(LOG_WARNING, "Audience is too large, skipping");
-                        }
+			if (strlen(audience->u.string) < MAX_RESP_FIELD) {
+				strcpy(browserid_response->audience, audience->u.string);
+			} else {
+				utils->log(NULL, SASL_LOG_WARN, "Audience is too large, skipping");
+			}
 		}
 
 		issuer = yajl_tree_get(tree, issuer_path, yajl_t_string);
 		if (!issuer) {
-			syslog(LOG_ERR, "Expected field issuer is missing");
+			utils->log(NULL, SASL_LOG_ERR, "Expected field issuer is missing");
 		} else {
-                        if (strlen(issuer->u.string) < MAX_RESP_FIELD) {
-                            strcpy(browserid_response->issuer, issuer->u.string);
-                        } else {
-                                syslog(LOG_WARNING, "Issuer is too large, skipping");
-                        }			
+			if (strlen(issuer->u.string) < MAX_RESP_FIELD) {
+				strcpy(browserid_response->issuer, issuer->u.string);
+			} else {
+				utils->log(NULL, SASL_LOG_WARN, "Issuer is too large, skipping");
+			}			
 		}
 
 		expires = yajl_tree_get(tree, expires_path, yajl_t_number);
 		if (!expires) {
-			syslog(LOG_INFO, "Expected field expires is missing or not a number");
+			utils->log(NULL, SASL_LOG_NOTE, "Expected field expires is missing or not a number");
 		} else {
 			browserid_response->expires = expires->u.number.i;
 		}
@@ -277,13 +280,13 @@ static int parse(const char* resp,
 		const char *reason_path[] = { "reason", (const char *) 0 };
 		yajl_val reason = yajl_tree_get(tree, reason_path, yajl_t_string);
 		if (!reason) {
-			syslog(LOG_ERR, "Expected field reason is missing");
+			utils->log(NULL, SASL_LOG_ERR, "Expected field reason is missing");
 		} else {
-                        if (strlen(reason->u.string) < MAX_RESP_FIELD) {
-                            strcpy(browserid_response->reason, reason->u.string);
-                        } else {
-                                syslog(LOG_WARNING, "BrowserID verifier failure reason is too large to copy, skipping.");
-                        }
+			if (strlen(reason->u.string) < MAX_RESP_FIELD) {
+				strcpy(browserid_response->reason, reason->u.string);
+			} else {
+				utils->log(NULL, SASL_LOG_WARN, "BrowserID verifier failure reason is too large to copy, skipping.");
+			}
 			
 		}
 		return SASL_FAIL;
